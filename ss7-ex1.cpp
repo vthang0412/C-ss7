a2d5bf30a3a70aae56996958a7b8c06158533723
vthang0412 <vthang041206@gmail.com>
2024-11-27 21:12:44 +0700

    Update ss7-ex1.cpp

--- ss7-ex1.cpp
+++ ss7-ex1.cpp
@@ -1,9 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-	int a[5]={1,2,3,4,5};
-		for (int i=0;i<5;i++){
-				printf("%d ",a[i]);
-		}
-			return 0;
-}
+int main() {
+    int a[5] = {1, 2, 3, 4, 5};  
+    int n = sizeof(a) / sizeof(a[0]);  
+    printf("Cac phan tu trong mang la:\n");
+	    for (int i = 0; i < n; i++) {
+	        printf("a[%d] = %d\n", i, a[i]);
+	    }	
+	    printf("Do dai cua mang la: %d\n", n);
+	
+	    return 0;
+	}
