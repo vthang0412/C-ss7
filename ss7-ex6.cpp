1ca6877efd8d9c57104aaed4b07c7eac8ffe8244
vthang0412 <vthang041206@gmail.com>
2024-11-27 21:22:27 +0700

    Add files via upload

---
+++ ss7-ex6.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a[5];
+    for (int i = 0; i < 5; i++) {
+        printf("Nhap so nguyen thu %d: ", i+1);
+        scanf("%d", &a[i]);
+    }
+     for (int i = 0; i < 5; i++){
+     	if( a[i] % 2!=0 ){
+        a[i] += 2;
+    	printf ("So %d la so le nen tang 2 don vi :%d\n",a[i]-2,a[i]);
+	}else{
+		a[i] += 3;
+		printf ("So %d la so chan nen tang 3 don vi :%d\n",a[i]-3,a[i]);
+	}
+        
+}       
+        return 0;
+}
